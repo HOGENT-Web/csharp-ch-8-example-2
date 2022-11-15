c5f223b9a898530a6e9fd505cb4bc5a82c027ebe
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-11-15 16:07:07 +0100

    Create Product

---
+++ src/Client/Products/Create.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BogusStore.Shared.Products;
+using Microsoft.AspNetCore.Components;
+
+namespace BogusStore.Client.Products;
+
+public partial class Create
+{
+    private ProductDto.Mutate product = new();
+    [Inject] public IProductService ProductService { get; set; } = default!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+
+    private async Task CreateProductAsync()
+    {
+        int productId = await ProductService.CreateAsync(product);
+        NavigationManager.NavigateTo($"product/{productId}");
+    }
+}
