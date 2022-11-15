ee3a1d15adbeab340fcb9539f60853d41dacdeb7
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-11-15 19:07:01 +0100

    Filter Products

--- src/Client/Products/Index.razor.cs
+++ src/Client/Products/Index.razor.cs
@@ -11,11 +11,26 @@ public partial class Index
     [Inject] public IProductService ProductService { get; set; } = default!;
     [Inject] public ISidepanelService Sidepanel { get; set; } = default!;
 
-    private IEnumerable<ProductDto.Index>? products;
+    [Parameter, SupplyParameterFromQuery] public string? Searchterm { get; set; }
+    [Parameter, SupplyParameterFromQuery] public int? Page { get; set; }
+    [Parameter, SupplyParameterFromQuery] public int? PageSize { get; set; }
+    [Parameter, SupplyParameterFromQuery] public decimal? MinPrice { get; set; }
+    [Parameter, SupplyParameterFromQuery] public decimal? MaxPrice { get; set; }
+    [Parameter, SupplyParameterFromQuery] public int? TagId { get; set; }
 
-    protected override async Task OnInitializedAsync()
+    private IEnumerable<ProductDto.Index>? products;
+    protected override async Task OnParametersSetAsync()
     {
-        ProductRequest.Index request = new();
+        ProductRequest.Index request = new()
+        {
+            Searchterm = Searchterm,
+            Page = Page.HasValue ? Page.Value : 1,
+            PageSize = PageSize.HasValue ? PageSize.Value : 25,
+            MinPrice = MinPrice,
+            MaxPrice = MaxPrice,
+            TagId = TagId
+        };
+
         var response = await ProductService.GetIndexAsync(request);
         products = response.Products;
     }
