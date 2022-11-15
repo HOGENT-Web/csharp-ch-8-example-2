463e670f0d001adba91e646697e2ea9eae1e253b
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-11-15 15:26:47 +0100

    Implement Delete

--- src/Client/Products/Detail.razor.cs
+++ src/Client/Products/Detail.razor.cs
@@ -10,10 +10,17 @@ public partial class Detail
     private ProductDto.Detail? product;
     [Parameter] public int Id { get; set; }
     [Inject] public IProductService ProductService { get; set; } = default!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
 
     protected override async Task OnParametersSetAsync()
     {
         var response = await ProductService.GetDetailAsync(Id);
         product = response;
+    }
+
+    private async Task DeleteProductAsync()
+    {
+        await ProductService.DeleteAsync(Id);
+        NavigationManager.NavigateTo("product");
     }
 }
